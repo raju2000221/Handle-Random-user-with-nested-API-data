884261dbf87d43d183ef6a697c8ca83072c72eb6
raju2000221 <bdrajuislam246@gmail.com>
2023-03-26 15:33:32 +0600

    final saved

--- js/random.js
+++ js/random.js
@@ -6,6 +6,9 @@ fetch('https://randomuser.me/api/?gender=female')
 const dispalayUser = user =>{
     const gendername = document.getElementById('name')
     const genderTag = document.getElementById('gender')
+    document.getElementById('city').innerHTML =user.results[0].location.city
+    document.getElementById('sa').innerHTML =user.results[0].location.state
+
     gendername.innerHTML =user.results[0].name.first
     genderTag.innerHTML = user.results[0].gender;
     
